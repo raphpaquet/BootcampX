a3c989e4408651be29049f49b55f22e893e3c0f9
raphpaquet <raphaelle.paquet@hotmail.com>
2020-11-23 17:45:27 +0000

    student without github account

---
+++ 0_selects/1_students_without_github.sql
@@ -0,0 +1,29 @@
+SELECT id, name, email, cohort_id
+FROM students
+WHERE github IS NULL
+ORDER BY cohort_id;
+
+
+
+ id  |       name        |             email              | cohort_id 
+-----+-------------------+--------------------------------+-----------
+  99 | Herminia Smitham  | sawayn.sarina@yahoo.com        |         7
+ 102 | Jacinthe Kautzer  | litzy_fay@hilpert.net          |         7
+ 111 | Bernardo Turcotte | margarita.anderson@paolo.name  |         8
+ 123 | Eloisa Quigley    | schmidt.ansel@gmail.com        |         9
+ 128 | Tiana Altenwerth  | zelda.stanton@yahoo.com        |         9
+ 146 | Hailie Kutch      | zora.corkery@goldner.net       |        10
+ 156 | Brook Fadel       | savanna.will@louisa.io         |        11
+ 159 | Shannon Tromp     | jayden_bahringer@hotmail.com   |        11
+ 160 | Aurore Yundt      |                                |        11
+ 163 | Kurt Turcotte     |                                |        11
+ 166 | Ike Lockman       | goodwin_zelda@gmail.com        |        11
+ 170 | Delores Gibson    | lehner_mustafa@sipes.com       |        11
+ 174 | Jeramie Volkman   | lucile.lynch@abbie.tv          |          
+ 176 | Florida Turner    | marks_roman@yahoo.com          |          
+ 177 | Darien Luettgen   | issac_collins@pink.io          |          
+ 178 | Javonte Ward      | jessie_howell@hotmail.com      |          
+ 183 | Marisa Rau        |                                |          
+ 184 | Hettie Hettinger  |                                |          
+ 188 | Holly Nitzsche    | lindsey.barrows@marcellus.info |          
+ 192 | Jerrold Lindgren  | jabari.stoltenberg@witting.biz | 
